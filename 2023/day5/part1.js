cab676fb67ebd67b2793e38d6a2956a39797e052
Isaac Martin <isaacmartin@Isaacs-MacBook-Air.local>
2023-12-05 00:01:09 -0500

    day 5

---
+++ 2023/day5/part1.js
@@ -0,0 +1,14 @@
+const fs = require("fs")
+
+let lines = fs.readFileSync("input.csv", {encoding: "utf-8"}).split("\n")
+
+for (let i=0; i<lines.length; i++) {
+    lines[i] = lines[i].slice(0,-1)
+}
+
+function main(lines) {
+    console.log(lines)
+
+}
+
+masterNumber = main(lines)
