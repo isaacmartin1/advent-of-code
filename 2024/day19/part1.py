e0adee65ddf5352b31bd1129a27e927083b65c9f
isaacmartin1 <iaaamartin@gmail.com>
2024-12-19 01:23:15 -0600

    feat: day 19

---
+++ 2024/day19/part1.py
@@ -0,0 +1,39 @@
+from functools import cache
+
+
+with open("input.csv", encoding="utf-8") as file:
+    lines = [line.split() for line in file.read().split("\n")]
+    combinations = []
+    patterns_list = []
+    passed_half = False
+    for line in lines:
+        if line == []:
+            passed_half = True
+        elif passed_half:
+            combinations.append(line)
+        else:
+            patterns_list = [item.replace(',','') for item in line]
+
+
+@cache
+def get_viability(combination):
+    if combination == "":
+        return 1
+    else:
+        for pattern in patterns_list:
+            if combination.startswith(pattern):
+                if get_viability(combination[len(pattern):]):
+                    return 1
+    return 0
+
+
+def main():
+    num_possible = 0
+    for combination in combinations:
+        num_possible += get_viability(combination[0])
+    return num_possible
+
+
+answer = main()
+
+print(answer)
