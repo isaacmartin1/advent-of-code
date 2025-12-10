3fc0447c4e85f26bd4c70cf1949f68edda91ee6a
Isaac Martin <iaaamartin@gmail.com>
2025-12-09 20:39:59 -0500

    fix: use a more efficient solution for part 2

--- 2025/day9/part2.py
+++ 2025/day9/part2.py
@@ -13,7 +13,7 @@ def main():
         x_coord, y_coord = [int(x) for x in line]
         list_of_coords.append([x_coord, y_coord])
 
-    # get a list of largest area and assoc coords
+    # get a list of largest area and associated coords
     biggest_areas = []
     for x_coord_1, y_coord_1 in list_of_coords:
         for x_coord_2, y_coord_2 in list_of_coords:
@@ -21,47 +21,50 @@ def main():
             biggest_areas.append([area, [[x_coord_1, y_coord_1], [x_coord_2, y_coord_2]]])
 
     biggest_areas.sort(reverse=True)
-
-    perimeter = []
-    for idx in range(len(list_of_coords)):
-        if idx != len(list_of_coords) - 1:
-            current_x, current_y = list_of_coords[idx]
-            next_x, next_y = list_of_coords[idx + 1]
-        else:
-            current_x, current_y = list_of_coords[idx]
-            next_x, next_y = list_of_coords[0]
-        larger_x, smaller_x = max(current_x, next_x), min(current_x, next_x)
-        larger_y, smaller_y = max(current_y, next_y), min(current_y, next_y)
-        x_diff = larger_x - smaller_x
-        y_diff = larger_y - smaller_y
-        perimeter.append([current_x, current_y])
-        if y_diff > 0:
-            for new_y in range(y_diff):
-                perimeter.append([smaller_x, new_y + smaller_y])
-        elif x_diff > 0:
-            for new_x in range(x_diff):
-                perimeter.append([new_x + smaller_x, smaller_y])
     
     for area, [[x_coord_1, y_coord_1], [x_coord_2, y_coord_2]] in biggest_areas:
         is_found = True
         larger_x, smaller_x = max(x_coord_1, x_coord_2), min(x_coord_1, x_coord_2)
         larger_y, smaller_y = max(y_coord_1, y_coord_2), min(y_coord_1, y_coord_2)
 
-        monitored_values = []
-        for x_coord, y_coord in perimeter:
-            if (x_coord == x_coord_1 and y_coord == y_coord_1) or (x_coord == x_coord_2 and y_coord == y_coord_2):
-                continue
-            elif smaller_x < x_coord < larger_x and smaller_y < y_coord < larger_y:
-                is_found = False
-                monitored_values.append([x_coord, y_coord])
+        for idx in range(len(list_of_coords)):
+            current_x, current_y = list_of_coords[idx]
+            next_x, next_y = list_of_coords[0] if idx + 1 == len(list_of_coords) else list_of_coords[idx + 1]
+
+            # VERTICAL LINE
+            if abs(next_y - current_y) > 0:
+                y_perimeter_min, y_perimeter_max = [next_y, current_y] if next_y < current_y else [current_y, next_y]
+                if smaller_x < current_x < larger_x:
+                    if y_perimeter_min < larger_y < y_perimeter_max or y_perimeter_min < smaller_y < y_perimeter_max:
+                        is_found = False
+                        break
+                    elif smaller_y < y_perimeter_min < larger_y or smaller_y < y_perimeter_max < larger_y:
+                        is_found = False
+                        break
+                    elif smaller_y == y_perimeter_min or larger_y == y_perimeter_max:
+                        is_found = False
+                        break
+
+            # HORIZONTAL LINE
+            elif abs(next_x - current_x) > 0:
+                x_perimeter_min, x_perimeter_max = [next_x, current_x] if next_x < current_x else [current_x, next_x]
+                import pdb
+                if smaller_y < current_y < larger_y:
+                    if x_perimeter_min < larger_x < x_perimeter_max or x_perimeter_min < smaller_x < x_perimeter_max:
+                        is_found = False
+                        break
+                    elif smaller_x < x_perimeter_min < larger_x or smaller_x < x_perimeter_max < larger_x:
+                        is_found = False
+                        break
+                    elif smaller_x == x_perimeter_min or larger_x == x_perimeter_max:
+                        is_found = False
+                        break
 
         if is_found == True:
             largest_area = area
             break
 
-
     return largest_area
 
 answer = main()
-
 print(answer)
